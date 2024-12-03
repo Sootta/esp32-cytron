e80714118741c99ae17d00fb8371052e521d8081
Sootta <147506050+Sootta@users.noreply.github.com>
2024-12-03 22:38:02 +0900

    ミス直し

--- src/esp32_cytron.h
+++ src/esp32_cytron.h
@@ -6,7 +6,7 @@
 
 class Esp32Cytron : public Motor {
     public:
-    Esp32Motor(int dir_pin, int pwm_channel) {
+    Esp32Cytron(int dir_pin, int pwm_channel) {
         this->dir_pin = dir_pin;
         this->pwm_channel = pwm_channel;
     }
@@ -18,12 +18,12 @@ class Esp32Cytron : public Motor {
 
     void ccw(int speed) {
         digitalWrite(dir_pin, HIGH);
-        ledcWrite(pwm_channel, speed)
+        ledcWrite(pwm_channel, speed);
     }
 
     private:
-    int dir_pin;
-    int pwm_channel;
+    int32_t dir_pin;
+    int32_t pwm_channel;
 };
 
 #endif
