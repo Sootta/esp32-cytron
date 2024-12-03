9ddfd6b94349cf3509109711724276a20fe83396
Sootta <147506050+Sootta@users.noreply.github.com>
2024-12-03 22:19:06 +0900

    esp32のコード

---
+++ src/esp32_motor.h
@@ -0,0 +1,29 @@
+#ifndef SOTA_ESP32_MOTOR
+#define SOTA_ESP32_MOTOR
+
+#include <Arduino.h>
+#include <roboken_interface.h>
+
+class Esp32Cytron : public Motor {
+    public:
+    Esp32Motor(int dir_pin, int pwm_channel) {
+        this->dir_pin = dir_pin;
+        this->pwm_channel = pwm_channel;
+    }
+
+    void cw(int speed) {
+        digitalWrite(dir_pin, LOW);
+        ledcWrite(pwm_channel, speed);
+    }
+
+    void ccw(int speed) {
+        digitalWrite(dir_pin, HIGH);
+        ledcWrite(pwm_channel, speed)
+    }
+
+    private:
+    int dir_pin;
+    int pwm_channel;
+};
+
+#endif
